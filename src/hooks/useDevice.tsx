6f16318cd774c4e9e34103675caf06d1d116e573
Montelukast <thestampmc@gmail.com>
2025-10-08 00:44:55 +0700

    Export DeviceType from useDevice hook
    DeviceType is now exported from the useDevice hook to allow usage in other modules that require knowledge of device type.

--- src/hooks/useDevice.tsx
+++ src/hooks/useDevice.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, useContext, useEffect, useState } from "react";
 
-type DeviceType = "mobile" | "tablet" | "desktop";
+export type DeviceType = "mobile" | "tablet" | "desktop";
 
 interface DeviceContextProps {
   deviceType: DeviceType;
